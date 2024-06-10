10962b95278bd3760d5ccc694cd021d9f5a84032
jin <manciepmc@gmail.com>
2024-06-10 14:22:33 +0900

    Initial Commit

---
+++ MoodShop/MoodShopApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoodShopApp.swift
+//  MoodShop
+//
+//  Created by 김진수 on 6/10/24.
+//
+
+import SwiftUI
+
+@main
+struct MoodShopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
