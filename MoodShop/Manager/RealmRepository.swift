e4dc477e440ee1fd35d94bd2e35814df2a7fb430
loveymelon <manciepmc@gmail.com>
2024-07-05 20:36:47 +0900

    Create Manager File

---
+++ MoodShop/Manager/RealmRepository.swift
@@ -0,0 +1,8 @@
+//
+//  RealmRepository.swift
+//  MoodShop
+//
+//  Created by 김진수 on 7/5/24.
+//
+
+import Foundation
---
+++ MoodShop/Protocol/RealmProtocol.swift
@@ -0,0 +1,8 @@
+//
+//  RealmProtocol.swift
+//  MoodShop
+//
+//  Created by 김진수 on 7/5/24.
+//
+
+import Foundation
