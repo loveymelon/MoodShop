a1102ab41d34f56ffd6b9c0b76c115de3214a6ca
loveymelon <manciepmc@gmail.com>
2024-07-09 15:31:52 +0900

    Create DetailMapper

--- MoodShop/Scene/Detail/Mapper/DetailMapper.swift
+++ MoodShop/Scene/Detail/Mapper/DetailMapper.swift
@@ -6,3 +6,22 @@
 //
 
 import Foundation
+
+final class DetailMapper {
+    
+    typealias Entity = ShopItemEntity
+    typealias DTO = ShopItemRequestDTO
+    
+    func entityToDTO(_ entity: Entity) -> DTO {
+        let imageURL = entity.image?.absoluteString ?? ""
+        
+        return ShopItemRequestDTO(title: entity.title, link: entity.link, image: imageURL, lprice: entity.lprice, mallName: entity.mallName, productId: entity.productId)
+    }
+    
+    func dtoToEntity(_ dto: DTO) -> Entity {
+        let imageURL = URL(string: dto.image)
+        
+        return ShopItemEntity(title: dto.title, link: dto.link, image: imageURL, lprice: dto.lprice, mallName: dto.mallName, productId: dto.productId)
+    }
+    
+}
