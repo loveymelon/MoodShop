daf7076d98885cb41019d25acf3068f86038adaa
loveymelon <manciepmc@gmail.com>
2024-07-17 22:29:28 +0900

    Create ShopItemListView, Container

--- MoodShop/Scene/ShopItemList/Container/ShopItemListContainer.swift
+++ MoodShop/Scene/ShopItemList/Container/ShopItemListContainer.swift
@@ -5,4 +5,82 @@
 //  Created by 김진수 on 7/17/24.
 //
 
-import Foundation
+import SwiftUI
+import Combine
+
+final class ShopItemListContainer: ObservableObject, ContainerProtocol {
+    
+    enum Intent {
+        case onAppear(String)
+        case nextPage(Int)
+        case network
+    }
+    
+    struct State {
+        var shopItems: [ShopItemEntity] = []
+        var error: String = ""
+        var shopItemTitles: [String] = []
+    }
+    
+    @Published
+    var state: State = State()
+    private var cancellables = Set<AnyCancellable>()
+    private var repository = HomeRepository()
+    private var title = ""
+    private var start = 1
+    private var total = 0
+    
+    func send(_ intent: Intent) {
+        switch intent {
+        case .onAppear(let text):
+            title = text
+            send(.network)
+        case .nextPage(let index):
+            if index + 2 >= state.shopItems.count  && index != total - 1 {
+                print("next")
+                start += 10
+                send(.network)
+            }
+        case .network:
+            Task {
+                await searchNetwork(text: title, start: String(start))
+            }
+        }
+    }
+}
+
+extension ShopItemListContainer {
+    private func searchNetwork(text: String, start: String, display: String = "10") async {
+        
+        await repository.fetchSearch(text: text, start: start, display: display)
+            .receive(on: RunLoop.main)
+            .sink(receiveCompletion: { [weak self] completion in
+                
+                guard let self else { return }
+                
+                switch completion {
+                case .finished:
+                    print("finish")
+                case .failure(let error):
+                    state.error = error.description
+                }
+            }, receiveValue: { [weak self] data in
+                
+                guard let self else { return }
+                
+                var temp: [String] = []
+                for item in data.items {
+                    temp.append(item.title.rmHTMLTag)
+                }
+                
+                print(data.start)
+                
+                total = data.total
+                
+                state.shopItemTitles.append(contentsOf: temp)
+                state.shopItems.append(contentsOf: data.items)
+                
+            }).store(in: &cancellables)
+        
+    }
+}
--- MoodShop/Scene/ShopItemList/View/ShopItemListView.swift
+++ MoodShop/Scene/ShopItemList/View/ShopItemListView.swift
@@ -6,13 +6,107 @@
 //
 
 import SwiftUI
+import Kingfisher
 
 struct ShopItemListView: View {
+    
+    var text: String
+    private let size = (UIScreen.main.bounds.width / 2) - 20
+    
+    @State
+    var selectedItem: ShopItemEntity?
+    @State
+    private var isDetailViewActive: Bool = false
+    @StateObject
+    private var container = ShopItemListContainer()
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ScrollView {
+            ScrollView(.horizontal) {
+                HStack {
+                    ForEach(ButtonTypeEnum.allCases, id: \.self) { title in
+                        makeButtons(title.rawValue)
+                    }
+                }
+                .padding(.leading, 8)
+            }
+            .padding(.vertical, 8)
+            
+            LazyVGrid(columns: [GridItem(.flexible()), GridItem(.flexible())]) {
+                makeItemList()
+            }
+        }
+        .onAppear {
+            container.send(.onAppear(text))
+        }
+        .navigationBarTitleDisplayMode(.inline)
+        .toolbar {
+            ToolbarItem(placement: .principal) {
+                Text("Mood Shop")
+                    .setTextStyle(size: 30, design: .serif, weight: .semibold)
+            }
+        }
+        
+        NavigationLink(
+            destination: DetailView(product: selectedItem),
+            isActive: $isDetailViewActive,
+            label: {
+                EmptyView()
+            }
+        )
+            
     }
 }
 
-#Preview {
-    ShopItemListView()
+extension ShopItemListView {
+    private func makeButtons(_ buttonTitle: String) -> some View {
+        Button {
+            print("tap")
+        } label: {
+            Text(buttonTitle)
+                .foregroundStyle(Color.black)
+        }
+        .padding(.all, 5)
+        .overlay {
+            RoundedRectangle(cornerRadius: 12)
+                .stroke(.black, lineWidth: 1)
+        }
+        .tag(/*@START_MENU_TOKEN@*/"Tag"/*@END_MENU_TOKEN@*/)
+        
+    }
+    
+    private func makeImage(_ imageURL: URL?, size: CGFloat) -> some View {
+        KFImage(imageURL)
+            .resizable()
+            .clipShape(RoundedRectangle(cornerRadius: 10))
+            .frame(width: size, height: size)
+            .shadow(color: .gray, radius: 4, x: 0, y: 0)
+    }
+    
+    private func makeItemList() -> some View {
+        ForEach(Array(container.state.shopItems.enumerated()), id: \.element.self) { index, item in
+            VStack(spacing: 5) {
+                makeImage(item.image, size: size)
+
+                VStack(alignment: .leading) {
+                    Text(item.mallName)
+                        .setTextStyle(size: 16, design: .default, weight: .bold)
+                    Text(container.state.shopItemTitles[index])
+                        .lineLimit(2)
+                    Text(String().addComma(to: item.lprice.description))
+                        .setTextStyle(size: 16, design: .default, weight: .bold)
+                }
+                .frame(width: size)
+                .padding(.leading, 5)
+                .onAppear {
+                    container.send(.nextPage(index))
+                }
+            }
+            .onTapGesture {
+                selectedItem = item
+                isDetailViewActive.toggle()
+            }
+            
+        }
+    }
 }
